f9ca6dd2dba0fc80d241f4e34e04c606965b23c6
nakiwalajosephine <josephinenakiwala25@gmail.com>
2026-04-14 13:12:30 +0300

    Update urls.py
    eat: implement DefaultRouter for movie, genre and person endpoints
    
    Integrated rest_framework.routers.DefaultRouter to automate URL routing.
    
    Registered ViewSets for MovieViewSet, GenreViewSet, and PersonViewSet.
    
    Simplified urlpatterns by replacing manual paths with standardized, auto-generated API routes.

--- backend/cinequest/urls.py
+++ backend/cinequest/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
+from . import views
+router = DefaultRouter()
+router.register(r"list", views.MovieViewSet, basename="movie")
+router.register(r"genres", views.GenreViewSet, basename="genre")
+router.register(r"people", views.PersonViewSet, basename="person")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
